c5f4f5371ed54a8640cafef3a13ff7ee521c3157
Munteanu Petrisor <munteanu_petrisor2710@yahoo.com>
2019-01-23 18:17:44 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <DynamicTable />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import DynamicTable from './components/DynamicTable.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            DynamicTable
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/DynamicTable.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Dynamic table</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DynamicTable',
+}
+</script>
